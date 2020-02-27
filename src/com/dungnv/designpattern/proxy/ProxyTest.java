7c22017aad4a01f19beb16b99b7f18f4ca57454c
abelegreen <abelegreen@gmail.com>
2020-02-27 15:42:23 +0700

    Improve example of proxy ppattern

--- src/com/dungnv/designpattern/proxy/ProxyTest.java
+++ src/com/dungnv/designpattern/proxy/ProxyTest.java
@@ -2,7 +2,10 @@ package com.dungnv.designpattern.proxy;
 
 public class ProxyTest {
 	public static void main(String[] args) {
-		RealObject obj = new RealObjectImplProxy();
+		RealObject obj = new RealObjectImplProxy(false);
 		obj.doSomething();
+		
+		RealObject obj2 = new RealObjectImplProxy(true);
+		obj2.doSomething();
 	}
 }
